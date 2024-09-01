143d8df4449db2d88541a9a6adba85c354bfad8f
Maaz Rana <115647670+MaazIcare@users.noreply.github.com>
2024-09-01 18:10:16 +0500

    removingBin

--- PracticeProject/obj/Debug/net6.0/PracticeProject.AssemblyInfo.cs
+++ PracticeProject/obj/Debug/net6.0/PracticeProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticeProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcdb95ff79e08ebd0bc319bc4e88d9c2b187c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticeProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticeProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
